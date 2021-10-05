8001f394ea55c71434a2041bcd75cbb3f9caff39
ankitbavne <you@example.com>
2021-10-05 15:48:39 +0530

    okkk

--- src/component/Register.js
+++ src/component/Register.js
@@ -12,118 +12,134 @@ const Register = () => {
   }
 
   return (
-     
     <div>
-<div className="container mt-3">
-      <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#" style={{fontSize:32}}>
-  PHONES</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div style={{textAlign:"right"}}>
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#" style={{fontSize:17}}>Smartphones</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#" style={{fontSize:17}}>Iphones</a>
-        </li>
-        <li class="nav-item"> 
-          <a class="nav-link"  href="#" style={{fontSize:17}}>Android</a>
-        </li>
-        {/* <li class="nav-item">
+      <div className="container mt-3">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#" style={{ fontSize: 32 }}>
+              PHONES
+            </a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div style={{ textAlign: "right" }}>
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a
+                    class="nav-link active"
+                    aria-current="page"
+                    href="#"
+                    style={{ fontSize: 17 }}
+                  >
+                    Smartphones
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#" style={{ fontSize: 17 }}>
+                    Iphones
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#" style={{ fontSize: 17 }}>
+                    Android
+                  </a>
+                </li>
+                {/* <li class="nav-item">
           <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
         </li> */}
-      </ul>
-    </div>
-  </div>
-</nav>
-    </div>
-
-
-
-
-    <div className="App">
-      <div className="container py-5">
-        <div
-          className="card border-0 shadow w-75 p-3 mx-auto "
-          style={{ backgroundColor: "lightblue", borderRadius: "20PX" }}
-        >
-          <h1 className="text-center pt-3 text-dark ">Ragistration Form</h1>
-          <form className="row g-3">
-            <div className="form-group">
-              <label htmlFor="fullname">Full Name</label>
-              <input
-                type="text"
-                className="form-control is-valid"
-                placeholder="FullName"
-                required
-                onChange={(e) => {
-                  setFullName(e.target.value);
-                }}
-                name="fullName"
-                value={fullName}
-              />
-              <div className="valid-feedback">Looks good!</div>
+              </ul>
             </div>
-            <div className="form-group">
-              <label htmlFor="mobile">Mobile</label>
-              <input
-                type="text"
-                className="form-control is-valid"
-                placeholder="Mobile"
-                required
-                onChange={(e) => {
-                  setMobile(e.target.value);
-                }}
-                name="mobile"
-                value={mobile}
-              />
-              <div className="valid-feedback">Looks good!</div>
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                className="form-control is-valid"
-                placeholder="Email"
-                required
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-                name="email"
-                value={email}
-              />
-              <div className="valid-feedback">Looks good!</div>
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">password</label>
-              <input
-                type="text"
-                className="form-control is-valid"
-                placeholder="Password"
-                required
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-                name="password"
-                value={password}
-              />
-              <div className="valid-feedback">Looks good!</div>
-            </div>
+          </div>
+        </nav>
+      </div>
 
-            <input
-              type="submit"
-              className="btn btn-primary my-8"
-              value="Register"
-              onClick={saveData}
-            />
-          </form>
+      <div className="App">
+        <div className="container py-5">
+          <div
+            className="card border-0 shadow w-75 p-3 mx-auto "
+            style={{ backgroundColor: "lightblue", borderRadius: "20PX" }}
+          >
+            <h1 className="text-center pt-3 text-dark ">Ragistration Form</h1>
+            <form className="row g-3">
+              <div className="form-group">
+                <label htmlFor="fullname">Full Name</label>
+                <input
+                  type="text"
+                  className="form-control is-valid"
+                  placeholder="FullName"
+                  required
+                  onChange={(e) => {
+                    setFullName(e.target.value);
+                  }}
+                  name="fullName"
+                  value={fullName}
+                />
+                <div className="valid-feedback">Looks good!</div>
+              </div>
+              <div className="form-group">
+                <label htmlFor="mobile">Mobile</label>
+                <input
+                  type="text"
+                  className="form-control is-valid"
+                  placeholder="Mobile"
+                  required
+                  onChange={(e) => {
+                    setMobile(e.target.value);
+                  }}
+                  name="mobile"
+                  value={mobile}
+                />
+                <div className="valid-feedback">Looks good!</div>
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  className="form-control is-valid"
+                  placeholder="Email"
+                  required
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                  name="email"
+                  value={email}
+                />
+                <div className="valid-feedback">Looks good!</div>
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">password</label>
+                <input
+                  type="text"
+                  className="form-control is-valid"
+                  placeholder="Password"
+                  required
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                  name="password"
+                  value={password}
+                />
+                <div className="valid-feedback">Looks good!</div>
+              </div>
+
+              <input
+                type="submit"
+                className="btn btn-primary my-8"
+                value="Register"
+                onClick={saveData}
+              />
+            </form>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
